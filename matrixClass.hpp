9ee316e76dff4e74ddb4b0aefcff8936368067fe
TannerSchultz2006 <tks92955@bethel.edu>
2023-10-19 14:55:05 -0500

    Change class to more inturpatable name mxTool -> matrix

--- matrixClass.hpp
+++ matrixClass.hpp
@@ -4,20 +4,20 @@
 Main class for matrix, future will call for multi-class
 Trying to limit external librarys to minimize any dependincies
 */
-class mxTool   {
+class matrix   {
 /*
-mxTool (int -> rows, int -> columns) -> matrix of rows*columns size
+matrix (int -> rows, int -> columns) -> matrix of rows*columns size
 matrixFill () -> fills matrix going across columns then down rows incramentin by 1
 matrixRead () -> std::cout's all values of the matrix
 transpose () -> transposes matrix flipping rows with columns and returning that value
 */
 
 public:
-        mxTool(int rows, int columns) { //defined for building the matrix
+        matrix(int rows, int columns) { //defined for building the matrix
                 rows++;
                 columns++;
-                mxTool::rows = rows;
-                mxTool::columns = columns;
+                matrix::rows = rows;
+                matrix::columns = columns;
                 Matrix = (int **) malloc (rows * sizeof(int *));
                 Matrix[0] = (int *) malloc((rows*columns)*sizeof(int));
                 for (int i = 1; rows > i; i++) {
@@ -31,20 +31,20 @@ public:
         void transpose();
 };
 
-void mxTool::transpose ()   {
-        mxTool tmpMatrix(columns-1, rows-1);
+void matrix::transpose ()   {
+        matrix tmpMatrix(columns-1, rows-1);
         for(int i = 1; i < rows; i++) {
                 for(int j = 1; j < columns; j++)
-                tmpMatrix.Matrix[j][i] = mxTool::Matrix[i][j];
+                tmpMatrix.Matrix[j][i] = matrix::Matrix[i][j];
         }
         free(Matrix[0]);
-        mxTool::Matrix = tmpMatrix.Matrix;
+        matrix::Matrix = tmpMatrix.Matrix;
         rows = tmpMatrix.rows;
         columns = tmpMatrix.columns;
 
 }
 
-void mxTool::matrixFill()   {
+void matrix::matrixFill()   {
         int count = 0;
         for (int i = 1; i < rows; i++) {
                 for (int j = 1; j < columns; j++) {
@@ -53,14 +53,15 @@ void mxTool::matrixFill()   {
         }
 };
 
-void mxTool::read ()   {
+void matrix::read ()   {
         std::cout << rows-1 << "x" << columns-1 << std::endl;
         for(int i = 1; i < rows; i++)   {
                 for(int j = 1; j < columns; j++)   {
-                        std::cout << mxTool::Matrix[i][j] << ", ";
+                        std::cout << matrix::Matrix[i][j] << ", ";
                 }
                 std::cout << std::endl;
         }
 };
 
-
+matrix dotProduct (matrix matrix0, matrix matrix1) {
+}
