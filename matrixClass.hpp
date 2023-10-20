9750a92b9cd82eea47504d5e72c0ccaf29c75a6e
TannerSchultz2006 <tks92955@bethel.edu>
2023-10-19 19:58:56 -0500

    Added dotproduct multiplication feature

--- matrixClass.hpp
+++ matrixClass.hpp
@@ -64,4 +64,21 @@ void matrix::read ()   {
 };
 
 matrix dotProduct (matrix matrix0, matrix matrix1) {
+        if (matrix0.columns != matrix1.rows) {
+                std::cout << "ERROR! These size matrix are not multiplyable";
+                matrix e(0, 0);
+                return e;
+        }
+        matrix1.transpose(); //This is increadably useful due to memory calls, trust me.
+        matrix tmpMatrix(matrix0.rows-1, matrix0.columns-1);
+        for (int row = 1; row < matrix0.rows; row++) {
+                for (int column = 1; column < matrix0.columns; column++) {
+                        int runningSum = 0;
+                        for (int i = 1; i < matrix1.rows; i++) {
+                                runningSum += matrix0.Matrix[row][i]*matrix1.Matrix[column][i];
+                        }
+                        tmpMatrix.Matrix[row][column] = runningSum;
+                }
+        }
+        return tmpMatrix;
 }
