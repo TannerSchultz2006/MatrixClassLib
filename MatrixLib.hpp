36e082c6edf6b6abcf910e5961af4a7591052ca6
TannerSchultz2006 <tks92955@bethel.edu>
2023-10-22 12:07:56 -0500

    Restructured files matrixClass.hpp -> MatrixLib.hpp split header into matrixLib + matrixClass.cpp added testMain.cpp

---
+++ MatrixLib.hpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"matrixFunctions.cpp"
+
+/*
+Main class for matrix, future will call for multi-class
+Trying to limit external librarys to minimize any dependincies
+matrix (int -> rows, int -> columns) -> matrix of rows*columns size
+matrixFill () -> fills matrix going across columns then down rows incramentin by 1
+matrixRead () -> std::cout's all values of the matrix
+transpose () -> transposes matrix flipping rows with columns and returning that value
+*/
+
+class matrix;
+matrix dotProduct (matrix matrix0, matrix matrix1); //See matrixFunctions.hpp
+matrix matrixSums (matrix matrix0, matrix matrix1);
---
+++ testMain.cpp
@@ -0,0 +1,7 @@
+#include"MatrixLib.hpp"
+
+int main () {
+        matrix X(4,4);
+        X.matrixFill();
+        X.read();
+}
